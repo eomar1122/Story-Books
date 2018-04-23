ca14d05767ca6e1159dfad93bb6dd94cb756e32e
Elshibani Omar <amr.omar80@gmail.com>
2018-04-22 18:48:38 -0600

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://root:password@ds251849.mlab.com:51849/story-books',
-  googleClientID: '608486097315-44v694ntu59lqdi270j4kjv74crrvmvm.apps.googleusercontent.com',
-  googleClientSecret: 'vIgtsVFhprQvaR1-0B3wpobl'
-}
